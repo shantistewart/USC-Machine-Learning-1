213c03d01fa313681a9b56185fd06566cbc1568f
Shanti Stewart <stewars2@oregonstate.edu>
2022-05-01 17:30:15 -0700

    Finished ModelPipeline class (for now).

--- final_project/models/model_pipeline_class.py
+++ final_project/models/model_pipeline_class.py
@@ -3,6 +3,7 @@
 
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 from final_project.preprocessing.load_data_class import DataLoader
 from final_project.preprocessing.preprocess_class import Preprocessor
 
@@ -15,12 +16,30 @@ class ModelPipeline:
             Mission 1: predict G1 and remove G2 and G3 features.
             Mission 2: predict G3 and remove G1 and G2 features.
             Mission 3: predict G3, while keeping G1 and G2 features.
-        model_pipe: sklearn pipeline object for model.
+        preprocessor: Preprocessor object.
+        model_pipe: sklearn Pipeline object for model.
     """
 
-    def __init__(self, mission):
+    def __init__(self, mission, model, norm_type="standard"):
+        """Initializes ModelPipeline object.
+
+        Args:
+            mission: (1, 2, or 3) Type of mission (task).
+                Mission 1: predict G1 and remove G2 and G3 features.
+                Mission 2: predict G3 and remove G1 and G2 features.
+                Mission 3: predict G3, while keeping G1 and G2 features.
+            model: sklearn model (estimator) object.
+            norm_type: Type of normalization to use.
+                allowed values: "standard"
+
+        Returns: None
+        """
+
         self.mission = mission
+        self.preprocessor = Preprocessor()
+        # make sklearn pipeline object:
         self.model_pipe = None
+        self.make_pipeline(model, norm_type=norm_type)
 
     def make_pipeline(self, model, norm_type="standard"):
         """Creates a sklearn model pipeline object.
@@ -48,14 +67,11 @@ class ModelPipeline:
             ("model", model)
         ])
 
-    def train(self, train_data_file, model, norm_type="standard"):
+    def train(self, train_data_file):
         """Trains model.
 
         Args:
             train_data_file: Name of data file.
-            model: sklearn model (estimator) object.
-            norm_type: Type of normalization to use.
-                allowed values: "standard"
 
         Returns: None
         """
@@ -64,11 +80,43 @@ class ModelPipeline:
         loader = DataLoader()
         X_orig, y_orig = loader.load_and_split_data(train_data_file, self.mission)
         # preprocess data:
-        prep = Preprocessor()
-        X_train, y_train = prep.preprocess_data(X_orig, y_orig, train=True)
+        X_train, y_train = self.preprocessor.preprocess_data(X_orig, y_orig, train=True)
 
-        # make sklearn pipeline object:
-        self.make_pipeline(model, norm_type=norm_type)
         # train model:
         self.model_pipe.fit(X_train, y_train)
+
+    def eval(self, data_file, verbose=1):
+        """Evaluates model on data.
+
+        Args:
+            data_file: Name of data file.
+            verbose: Nothing printed (0), accuracy and macro F1-score printed (1), all metrics printed (2)
+
+        Returns:
+            accuracy: Subset accuracy.
+            macro_f1: Macro F1-score.
+            conf_matrix: Confusion matrix.
+        """
+
+        # load data:
+        loader = DataLoader()
+        X_orig, y_orig = loader.load_and_split_data(data_file, self.mission)
+        # preprocess data:
+        X, y = self.preprocessor.preprocess_data(X_orig, y_orig, train=False)
+
+        # predict on data:
+        y_pred = self.model_pipe.predict(X)
+
+        # compute metrics (subset accuracy, macro F1-score, confusion matrix):
+        accuracy = accuracy_score(y, y_pred)
+        macro_f1 = f1_score(y, y_pred, average="macro")
+        conf_matrix = confusion_matrix(y, y_pred, normalize=None)
+        # print metrics:
+        if verbose == 1 or verbose == 2:
+            print("accuracy = {} %".format(100*accuracy))
+            print("macro F1-score = {}".format(macro_f1))
+        if verbose == 2:
+            print("confusion matrix = \n{}".format(conf_matrix))
+
+        return accuracy, macro_f1, conf_matrix
 
