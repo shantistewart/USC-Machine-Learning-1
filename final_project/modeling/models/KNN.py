fb2305bed364c94f7cb9334de7a0749e62edfe71
Shanti Stewart <stewars2@oregonstate.edu>
2022-05-03 16:30:28 -0700

    Updated KNN script.

--- final_project/modeling/models/KNN.py
+++ final_project/modeling/models/KNN.py
@@ -10,6 +10,8 @@ from final_project.modeling.run_missions import run_missions
 # data file names:
 train_data_file = "../../data/student_performance_train.csv"
 test_data_file = "../../data/student_performance_test.csv"
+# number of features (minimum over all missions):
+N_FEATURES = 43
 
 
 print()
@@ -20,32 +22,45 @@ norm_type = "standard"
 # feature selection method:
 feature_select = "KBest"
 # whether to use PCA:
-pca = False
+pca = True
 # number of folds (K) to use in stratified K-fold cross validation:
-n_folds = 10
+n_folds = 5
 
 
 # hyperparameter search method and scoring metric:
-search_type = "grid"
+search_type = "random"
+n_iters = 1000
 metric = "f1_macro"
 # hyperparameter values to search over:
 # type of weight function for KNN:
 weight_types = ["uniform", "distance"]
 # number of nearest neighbors for KNN:
-K_values = np.arange(start=0, stop=21, step=5)
-K_values[0] = 1
+K_values = np.arange(start=1, stop=31, step=1)
 # scoring function for SelectKBest feature selection:
 kbest_scores = [f_classif, mutual_info_classif]
 # number of features to keep for SelectKBest feature selection:
-n_features_select = np.arange(start=5, stop=41, step=5)
+n_features_select = np.arange(start=1, stop=N_FEATURES+1, step=1)
+# fraction of variance required to be explained by result of PCA:
+pca_var = np.linspace(0.25, 1.0, num=16)
+pca_var[len(pca_var)-1] = 0.99
 
-hyperparams = {"model__weights": weight_types,
-               "model__n_neighbors": K_values,
-               "selector__score_func": kbest_scores,
-               "selector__k": n_features_select}
+if pca:
+    hyperparams = {"model__weights": weight_types,
+                   "model__n_neighbors": K_values,
+                   "selector__score_func": kbest_scores,
+                   "selector__k": n_features_select,
+                   "pca__svd_solver": ["full"],
+                   "pca__n_components": pca_var,
+                   "pca__whiten": [True]}
+else:
+    hyperparams = {"model__weights": weight_types,
+                   "model__n_neighbors": K_values,
+                   "selector__score_func": kbest_scores,
+                   "selector__k": n_features_select}
 print("Hyperparameter search values:\n{}".format(hyperparams))
 
 # run all missions:
 run_missions(train_data_file, test_data_file, model, norm_type=norm_type, feature_select=feature_select, pca=pca,
-             hyper_params=hyperparams, search_type=search_type, metric=metric, n_folds=n_folds, verbose=2)
+             hyper_params=hyperparams, search_type=search_type, metric=metric, n_iters=n_iters, n_folds=n_folds,
+             verbose=2)
 
